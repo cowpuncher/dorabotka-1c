efef67197d2ca076379e66c7933c8c45e0b02dd0
WebDev TVlean <tvleanjohnny@gmail.com>
2022-02-15 18:02:08 +0300

    add section how work on main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    //Partners slider
     const partnersSlider = new Swiper('.partnersSlider', { 
-        slidesPerView: 6,
-        watchOverflow: true, 
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+      slidesPerView: 6,
+      watchOverflow: true, 
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },      
+    });
+
+    //Review slider 
+    const reviewSlider = new Swiper('.reviewSlider', { 
+      slidesPerView: 1,
+      watchOverflow: true, 
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },      
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '"></span>';
         },
-      
-      });
-
-
+      },
+    });
+    // add Image preview
+    let previewBulletImages = document.querySelectorAll('.previewBulletImages img');
+    let previewBulletSpan = document.querySelectorAll('.swiper-pagination span');
+    for(var i = 0; i < previewBulletSpan.length; i++) {
+      previewBulletSpan[i].appendChild(previewBulletImages[i]);
+    }
+    //----------------------------------------------------------
     // Text animated
     const changeText = (outer, inner, time, speed) => {
         let typed = new Typed(outer, {
