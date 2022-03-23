7beb962a111203884f701a1432bc577f6a9fdd57
WebDev TVlean <tvleanjohnny@gmail.com>
2022-03-23 17:14:49 +0300

    add page orders and balance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,104 @@
+jQuery(document).ready(function ($) {
+ 
+  var maxFileSize = 2 * 1024 * 1024; // (байт) Максимальный размер файла (2мб)
+  var queue = {};
+  var form = $('form#uploadImages');
+  var imagesList = $('#uploadImagesList');
+
+  var itemPreviewTemplate = imagesList.find('.item.template').clone();
+  itemPreviewTemplate.removeClass('template');
+  imagesList.find('.item.template').remove();
+
+
+  $('#addImages').on('change', function () {
+      var files = this.files;
+
+      for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+
+          if ( !file.type.match(/image\/(jpeg|jpg|png|gif)/) ) {
+              alert( 'Фотография должна быть в формате jpg, png или gif' );
+              continue;
+          }
+
+          if ( file.size > maxFileSize ) {
+              alert( 'Размер фотографии не должен превышать 2 Мб' );
+              continue;
+          }
+
+          preview(files[i]);
+      }
+
+      this.value = '';
+  });
+
+  // Создание превью
+  function preview(file) {
+      var reader = new FileReader();
+      reader.addEventListener('load', function(event) {
+          var img = document.createElement('img');
+
+          var itemPreview = itemPreviewTemplate.clone();
+
+          itemPreview.find('.img-wrap img').attr('src', event.target.result);
+          itemPreview.data('id', file.name);
+
+          imagesList.append(itemPreview);
+
+          queue[file.name] = file;
+
+      });
+      reader.readAsDataURL(file);
+  }
+
+  // Удаление фотографий
+  imagesList.on('click', '.delete-link', function () {
+      var item = $(this).closest('.item'),
+          id = item.data('id');
+
+      delete queue[id];
+
+      item.remove();
+  });
+
+
+  // Отправка формы
+  form.on('submit', function(event) {
+
+      var formData = new FormData(this);
+
+      for (var id in queue) {
+          formData.append('images[]', queue[id]);
+      }
+
+      $.ajax({
+          url: $(this).attr('action'),
+          type: 'POST',
+          data: formData,
+          async: true,
+          success: function (res) {
+              alert(res)
+          },
+          cache: false,
+          contentType: false,
+          processData: false
+      });
+
+      return false;
+  });
+
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
+  
+  // addFile = document.querySelector('.addFile');
+  orderFile = document.getElementById('orderFile');
+
+  orderFile.addEventListener('change', e => {
+    console.log(orderFile.value);
+  })
     //Partners slider
     const partnersSlider = new Swiper('.partnersSlider', { 
       slidesPerView: 6,
@@ -62,10 +161,11 @@ document.addEventListener('DOMContentLoaded', function() {
     changeText('#animateChatItemFirst', '#animateChatItem1', 0 , 1);
     changeText('#animateChatItemSecond', '#animateChatItem2', 1500 , 1);
     changeText('#animateChatItemThird', '#animateChatItem3', 3000, 1);
+
  }, false);
 
-
-jQuery(function($){
-    $('.table').footable();
+ jQuery(function($){
+  $('.table').footable();
 });
+
 
